932f608eaab763f4221dabda337fbd9747222cb7
Marc Nischan <marcnischan@gmail.com>
2015-05-06 10:27:16 -0400

    Adding code to scroll the alphabet

--- app/js/controllers/game.ctrl.js
+++ app/js/controllers/game.ctrl.js
@@ -2,7 +2,11 @@ angular.module('app').controller('GameCtrl', GameCtrl);
 
 function GameCtrl(letterList) {
 	var game = this;
-	game.sidebar = "Put the sidebar here.";
 	game.letterSet = letterList.currentAlphabet;
 	game.toggleLetterOff = letterList.toggleLetterOff;
+	game.scrollOffset = 0;
+	game.scroll =  function(){
+		game.scrollOffset = letterList.scroll(game.scrollOffset);
+		console.log(game.scrollOffset);
+	};
 }
--- app/js/services/letterlist.factory.js
+++ app/js/services/letterlist.factory.js
@@ -6,11 +6,24 @@
 		letters.alphabetInit = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 		letters.currentAlphabet = letters.alphabetInit;
 		letters.toggleLetterOff = function(index){
-			console.log("You toggled me off!");
 			letters.currentAlphabet.splice(index,1);
-
-
-			console.log("You toggled me off!" + letters.currentAlphabet);
+		};
+		letters.scroll = function(currentIndex){
+			console.log("You clicked scroll " + currentIndex);
+			switch(currentIndex){
+				case 0:
+					currentIndex = 300;
+					break;
+				case 300:
+					currentIndex = 600;
+					break;
+				case 600:
+					currentIndex = 0;
+					break;
+				default:
+					currentIndex = 0;
+			}
+			return currentIndex;
 		};
 
 		return letters;
